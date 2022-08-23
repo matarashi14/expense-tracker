72b8108f7f4ad9a3eff5d40fc42a7a8e7c19b4f1
Motoki Atarashi <motoki@Motokis-MacBook-Air-2.local>
2022-08-22 20:50:13 -0700

    complete section3

---
+++ src/components/UI/Card.js
@@ -0,0 +1,11 @@
+
+import './Card.css'
+
+function Card(props) {
+    const styleClass = 'card ' + props.className;
+    return (
+        <div className={styleClass}>{props.children}</div>
+    )
+}
+
+export default Card
